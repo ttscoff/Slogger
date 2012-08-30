ac1d93bc31b32fcc1d451f25c9e8428718a97144
Dom Barnes <dom@dombarnes.com>
2012-08-30 16:26:53 +0100

    Removed Starred entries and fixed blank entries

--- plugins/getgluelogger.rb
+++ plugins/getgluelogger.rb
@@ -3,23 +3,22 @@ Plugin: GetGlue Logger
 Description: Brief description (one line)
 Author: [Dom Barnes](http://dombarnes.com)
 Configuration:
-  option_1_name: [ "example_value1" , "example_value2", ... ]
-  option_2_name: example_value
+  getglue_feed: "http://feeds.getglue.com/checkins/AbCDeFG_HijK"
+  getglue_tags "@social @entertainment"
 Notes:
-  - multi-line notes with additional description and information (optional)
+  - Retrieve this from your GetGlue profile page (http://getglue.com/username). You will need to view source to find this.
 =end
 
-config = { # description and a primary key (username, url, etc.) required
+config = {
   'description' => ['GetGlue logger grabs all your activity including checkins, likes and stickers',
                     'You will need the RSS feed of your Activity stream.'],
-  'getglue_username' => 'getglue', # update the name and make this a string or an array if you want to handle multiple accounts.
   'getglue_feed' => "",
-  'tags' => '@social @entertainment' # A good idea to provide this with an appropriate default setting
+  'tags' => '@social @entertainment'
 }
-# Update the class key to match the unique classname below
+
 $slog.register_plugin({ 'class' => 'GetglueLogger', 'config' => config })
 
-# unique class name: leave '< Slogger' but change ServiceLogger (e.g. LastFMLogger)
+
 class GetglueLogger < Slogger
   # every plugin must contain a do_log function which creates a new entry using the DayOne class (example below)
   # @config is available with all of the keys defined in "config" above
@@ -32,9 +31,6 @@ class GetglueLogger < Slogger
       if !config.key?('getglue_username') || config['getglue_username'] == []
         @log.warn("GetGlue has not been configured or an option is invalid, please edit your slogger_config file.")
         return
-      else
-        # set any local variables as needed
-        username = config['getglue_username']
       end
     else
       @log.warn("GetGlue has not been configured or a feed is invalid, please edit your slogger_config file.")
@@ -46,7 +42,7 @@ class GetglueLogger < Slogger
     tags = config['tags'] || ''
     tags = "\n\n#{@tags}\n" unless @tags == ''
 
-    today = @timespan
+    today = @timespan.to_i
 
     # Perform necessary functions to retrieve posts
     entrytext = ''
@@ -72,21 +68,18 @@ class GetglueLogger < Slogger
 
     # create an options array to pass to 'to_dayone'
     # all options have default fallbacks, so you only need to create the options you want to specify
-    options = {}
-    options['content'] = "## GetGlue Activity for #{@timespan.strftime('%m-%d-%Y')}\n\n#{content} #{tags}"
-    options['datestamp'] = Time.now.utc.iso8601
-    options['starred'] = true
-    options['uuid'] = %x{uuidgen}.gsub(/-/,'').strip
+    if content != ''
+      options = {}
+      options['content'] = "## GetGlue Activity for #{@timespan.strftime('%m-%d-%Y')}\n\n#{content} #{tags}"
+   #   options['datestamp'] = @timespan.
+      options['uuid'] = %x{uuidgen}.gsub(/-/,'').strip
 
-    # Create a journal entry
-    # to_dayone accepts all of the above options as a hash
-    # generates an entry base on the datestamp key or defaults to "now"
-    sl = DayOne.new
-    sl.to_dayone(options)
-
-    # To create an image entry, use `sl.to_dayone(options) if sl.save_image(imageurl,options['uuid'])`
-    # save_image takes an image path and a uuid that must be identical the one passed to to_dayone
-    # save_image returns false if there's an error
-
+       # Create a journal entry
+       # to_dayone accepts all of the above options as a hash
+       # generates an entry base on the datestamp key or defaults to "now"
+      
+      sl = DayOne.new
+      sl.to_dayone(options)
+    end
   end
 end
