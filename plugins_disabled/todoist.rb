3f10713316b14f7d28799e897f54677b099a23f7
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-02-22 15:55:04 +0000

    Added TodoistLogger class

--- plugins_disabled/todoist.rb
+++ plugins_disabled/todoist.rb
@@ -1,1 +1,20 @@
+=begin
+Plugin: Todoist
+Description: Logs completed todos from Todoist
+Notes: Thanks go to Brian Stearns who inspired me to create this given his
+       `Things.rb` plugin.
+Author: [Freddie Lindsey](twitter.com/freddielindsey)
+=end
 
+config = {
+  'todoist_description' => [
+    'Logs completed todos from Todoist'
+  ],
+  'todoist_tags' => '#todos',
+  'todoist_save_hashtags' => true
+}
+
+$slog.register_plugin({ 'class' => 'TodoistLogger', 'config' => config })
+
+class TodoistLogger < Slogger
+end
