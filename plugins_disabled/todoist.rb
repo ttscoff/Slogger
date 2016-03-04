081dab6a26240d5a9b61e425fe2f3f9fdd22fa68
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-03-04 16:07:15 +0000

    Finished draft with compatibility for -t

--- plugins_disabled/todoist.rb
+++ plugins_disabled/todoist.rb
@@ -9,8 +9,11 @@ config = {
     'Logs completed todos from Todoist'
   ],
   todoist_token: '6bdc699af159db1b1e1148af84c8ecaf79cfe659',
-  todoist_tags: '#todos',
-  todoist_save_hashtags: true
+  todoist_tags: [
+   'todos'
+  ], # No need to include hashes here
+  todoist_save_hashtags: true,
+  todoist_item_limit: 2
 }
 
 $slog.register_plugin({ 'class' => 'TodoistLogger', 'config' => config })
@@ -34,28 +37,14 @@ class TodoistLogger < Slogger
     timespan = @timespan.strftime('%d/%m/%Y')
     output = ''
 
-    begin
-      url = URI('https://todoist.com/API/v6/get_all_completed_items')
-      time_ = Time.new(@timespan.year, @timespan.month, @timespan.day)
-      since = time_.strftime('%Y-%m-%dT%H:%M')
-      params = {
-        token: config[:todoist_token],
-        limit: 50,
-        since: since
-      }
-      url.query = URI.encode_www_form(params)
-
-      res = Net::HTTP.get_response(url)
-    rescue Exception => e
-      @log.error("ERROR retrieving Todoist information: #{url}")
-      return false
+    if  !config[:todoist_item_limit] ||
+        config[:todoist_item_limit] > 50 ||
+        config[:todoist_item_limit] < 1
+      config[:todoist_item_limit] = 50
     end
 
-    return false unless res.is_a?(Net::HTTPSuccess)
-    json = JSON.parse(res.body)
-
-    items = json['items']
-    projects = json['projects']
+    valid, items, projects = get_todoist_items(config)
+    return valid if !valid
 
     entries_by_day = split_by_day(items)
     entries = []
@@ -70,12 +59,64 @@ class TodoistLogger < Slogger
       options = {}
       options['title'] = "Todos completed on #{e[:day]}"
       options['content'] = e[:content]
+      options['tags'] = config[:todoist_tags]
       options['datestamp'] = e[:datestamp].utc.iso8601 if e[:datestamp]
       sl = DayOne.new
       sl.to_dayone(options)
     end
 
     @log.info("Todoist logged #{count} #{count > 1 ? 'entries' : 'entry'}")
+  end
+
+  def get_todoist_items(config)
+    offset = 0
+    items = []
+    projects = {}
+    time_ = Time.new(@timespan.year, @timespan.month, @timespan.day)
+    since = time_.strftime('%Y-%m-%dT%H:%M')
+
+    while true
+      begin
+        url = URI('https://todoist.com/API/v6/get_all_completed_items')
+        params = {
+          token: config[:todoist_token],
+          limit: config[:todoist_item_limit],
+          since: since,
+          offset: offset
+        }
+        url.query = URI.encode_www_form(params)
+
+        res = Net::HTTP.get_response(url)
+      rescue Exception => e
+        @log.error("ERROR retrieving Todoist information: #{url}")
+        return false, nil, nil
+      end
+
+      return false unless res.is_a?(Net::HTTPSuccess)
+      json = JSON.parse(res.body)
+
+      break if json['items'].length == 0
+      break if items.select{ |item|
+        item['task_id'] == json['items'][0]['task_id']
+      }.length > 0
+
+      items += json['items']
+      json['projects'].each do |k, v|
+        if projects[k]
+          unless projects[k] == v
+            @log.error("ERROR concurrent modification of Todoist information")
+            return false, nil, nil
+          end
+        else
+          projects[k] = v
+        end
+      end
+      offset += config[:todoist_item_limit]
+    end
+
+    @log.info("Retrieved #{items.length} items in #{(offset / config[:todoist_item_limit]) + 1} requests")
+
+    return true, items, projects
   end
 
   def get_project(projects, id)
@@ -116,8 +157,6 @@ class TodoistLogger < Slogger
         entry += "- #{item['content']}\n"
       end
     end
-
-    entry += "\n\n(#{config["TodoistLogger"][:todoist_tags]})"
 
     entry = {
       content: entry,
